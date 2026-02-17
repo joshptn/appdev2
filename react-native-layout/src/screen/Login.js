11aa4002d3da9eb6e4d877627fe65ff3b27dcef0
auhsojp <jashuapatenia15@gmail.com>
2026-02-18 03:55:07 +0800

    Install Ionicons and style Body Content

--- react-native-layout/src/screen/Login.js
+++ react-native-layout/src/screen/Login.js
@@ -1,24 +1,35 @@
 import React from 'react';
 import { View, Text, TextInput, TouchableOpacity, Image, StyleSheet } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
 
 export default function Login() {
   return (
     <View style={styles.container}>
 
-      {/* Header */}
-      <View style={styles.header}>
-        <Image source={require('../../assets/login.png')} style={styles.logo} />
-        <Text style={styles.title}>Log In</Text>
-      </View>
+        {/* Header */}
+        <View style={styles.header}>
+            <Image source={require('../../assets/login.png')} style={styles.logo} />
+            <Text style={styles.title}>Log In</Text>
+        </View>
 
-      {/* Body */}
-      <View style={styles.contentSection}>
-      </View>
+        {/* Body */}
+        <View style={styles.body}>
+            <View style={styles.inputContainer}>
+                <Ionicons name="mail-outline" size={20} style={styles.icon} />
+                <TextInput  placeholder="Email Address" style={styles.input} 
+            />
+            </View>
+            
+            <View style={styles.inputContainer}>
+                <Ionicons name="lock-closed-outline" size={20} style={styles.icon} />
+                <TextInput placeholder="Password" style={styles.input} />
+            </View>
+        </View>
 
-      {/* Footer */}
-      <View style={styles.footer}>
-      </View>
+        {/* Footer */}
+        <View style={styles.footer}>
 
+        </View>
     </View>
   );
 }
@@ -49,10 +60,32 @@ const styles = StyleSheet.create({
         marginTop: 130
     },
 
-    contentSection: {
+    body: {
         flex: 1.5,
-        borderColor: 'black',
-        borderWidth: 5,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+
+    inputContainer: {
+        flexDirection: 'row',
+        borderWidth: 2,
+        borderColor: '#1a0b2e',
+        borderRadius: 35,
+        marginBottom: 15,
+        width: '85%',
+        paddingHorizontal: 15,
+        backgroundColor: '#f5e3ff',
+        alignItems: 'center'
+    },
+
+    input:{ 
+        height: 50,
+        flex: 1
+    },
+
+    icon: {
+        color: "#4910e6",
+        marginRight: 10
     },
 
     footer: {
@@ -60,5 +93,6 @@ const styles = StyleSheet.create({
         borderColor: 'black',
         borderWidth: 5,
     },
+
 
 });
